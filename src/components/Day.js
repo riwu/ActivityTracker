30719c0be1843a3bc9726ca800dc29f5531bd376
riwu <riwu0730@gmail.com>
2017-12-11 03:33:20 +0800

    add removal of day record

--- src/components/Day.js
+++ src/components/Day.js
@@ -13,29 +13,34 @@ class Day extends Component {
         [index]: new Animated.ValueXY(),
       }), {}),
       selectedImageIndex: props.navigation.state.params.replace,
+      selectedImagePos: new Animated.ValueXY(),
     };
 
-    this.panResponders = images.map((image, index) => PanResponder.create({
+    const createPanResponder = index => PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: Animated.event([null, {
-        dx: this.state[index].x,
-        dy: this.state[index].y,
+        dx: index === undefined ? this.state.selectedImagePos.x : this.state[index].x,
+        dy: index === undefined ? this.state.selectedImagePos.y : this.state[index].y,
       }]),
       onPanResponderRelease: (e, gesture) => {
         const box = this.dropZone;
         const boxY = box.y + CONSTANTS.STATUS_BAR_HEIGHT + CONSTANTS.NAV_BAR_HEIGHT + props.extraHeight;
-        if (box.x <= gesture.moveX && gesture.moveX <= box.x + box.width &&
-          boxY <= gesture.moveY && gesture.moveY <= boxY + box.height) {
+        if ((box.x <= gesture.moveX && gesture.moveX <= box.x + box.width &&
+             boxY <= gesture.moveY && gesture.moveY <= boxY + box.height)
+             === (index !== undefined)) {
           this.setState({ selectedImageIndex: index });
           props.navigation.state.params.onChange(index);
         }
 
         Animated.decay(
-          this.state[index],
+          index === undefined ? this.state.selectedImagePos : this.state[index],
           { toValue: { x: 0, y: 0 } },
         ).start();
       },
-    }));
+    });
+
+    this.panResponders = images.map((image, index) => createPanResponder(index));
+    this.selectedImagePanResponder = createPanResponder(undefined);
   }
 
   renderContainer() {
@@ -44,7 +49,11 @@ class Day extends Component {
         <View onLayout={(e) => { this.dropZone = e.nativeEvent.layout; }} style={this.props.styles.box}>
           {
             this.state.selectedImageIndex === undefined ? null :
-            <Image style={this.props.styles.enlargedImage} source={this.props.images[this.state.selectedImageIndex]} />
+            <Animated.Image
+              {...this.selectedImagePanResponder.panHandlers}
+              style={[this.state.selectedImagePos.getLayout(), this.props.styles.enlargedImage]}
+              source={this.props.images[this.state.selectedImageIndex]}
+            />
           }
         </View>
       </View>
