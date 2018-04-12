d7cc8c6bb573414ccfa71a36dfdc7df9b222a798
riwu <riwu0730@gmail.com>
2018-04-12 22:45:28 +0800

    add credits

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,23 +1,51 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
-import CONSTANTS from '../constants';
-
-class Credits extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Credits</Text>
-      </View>
-    );
-  }
-}
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  label: {
+    fontSize: 20,
+  },
+  linkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  link: {
+    textDecorationLine: 'underline',
+    marginRight: 2,
+    fontSize: 20,
+  },
+  top: {
+    marginBottom: 50,
   },
 });
+
+const Credits = () => (
+  <View style={styles.container}>
+    <Text style={styles.label}>App is developed by:</Text>
+    <TouchableOpacity
+      style={[styles.linkContainer, styles.top]}
+      onPress={() => Linking.openURL('https://wangriwu.com/Contact')}
+    >
+      <Text style={styles.link}>Wang Riwu</Text>
+      <Feather name="external-link" />
+    </TouchableOpacity>
+
+    <Text style={styles.label}>Services and concept provided by:</Text>
+    <TouchableOpacity
+      style={styles.linkContainer}
+      onPress={() => Linking.openURL('mailto:wfervin@gmail.com')}
+    >
+      <Text style={styles.link}>Wahidah Fervin</Text>
+      <MaterialCommunityIcons name="email-outline" />
+    </TouchableOpacity>
+  </View>
+);
 
 export default Credits;
