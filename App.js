0aec75ebdcd04f082a5847496a3d9ecc94f0b284
riwu <riwu0730@gmail.com>
2018-04-13 22:08:19 +0800

    check for update when app opens

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'remote-redux-devtools';
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage, AppState } from 'react-native';
 import { persistStore, persistReducer } from 'redux-persist';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import {
   createReactNavigationReduxMiddleware,
   createReduxBoundAddListener,
 } from 'react-navigation-redux-helpers';
+import { Updates } from 'expo';
 
 import reducer from './src/reducers';
 import AppWithNavigationState from './src/navigators/AppNavigator';
@@ -36,5 +37,12 @@ const App = () => (
     </PersistGate>
   </Provider>
 );
+
+AppState.addEventListener('change', async () => {
+  const { isAvailable } = await Updates.checkForUpdateAsync();
+  if (!isAvailable) return;
+  await Updates.fetchUpdateAsync();
+  Updates.reload();
+});
 
 export default App;
