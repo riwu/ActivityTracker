a63839b6d170dafbaef742d2febc0c5bd42f33a3
riwu <riwu0730@gmail.com>
2018-04-15 00:41:31 +0800

    add redux-persist migration config

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'remote-redux-devtools';
 import { AsyncStorage, AppState, Alert } from 'react-native';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, createMigrate } from 'redux-persist';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import {
   createReactNavigationReduxMiddleware,
@@ -14,9 +14,18 @@ import { Updates } from 'expo';
 import reducer from './src/reducers';
 import AppNavigator from './src/navigators/AppNavigator';
 
+const migrations = {
+  0: (state) => {
+    const { navigation, ...rest } = state;
+    return rest;
+  },
+};
+
 const config = {
   key: 'root',
   storage: AsyncStorage,
+  version: 0,
+  migrate: createMigrate(migrations),
 };
 
 const middleware = createReactNavigationReduxMiddleware('root', (state) => state.navigation);
