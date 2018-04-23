8ba79e80ff784ec32c31a79b8ee9af3fa97df4b2
riwu <riwu0730@gmail.com>
2018-04-23 20:50:52 +0800

    remove props.navigation.setParams in Chart to prevent re-rendering

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -41,101 +41,98 @@ const styles = StyleSheet.create({
 
 const DefaultItem = ({ main }) => <Text style={styles.text}>{main}</Text>;
 
-class Chart extends React.Component {
-  static navigationOptions = ({ navigation }) => ({
-    headerRight: (
-      <MaterialCommunityIcons
-        name="restart"
-        onPress={() => navigation.state.params.reset()}
-        size={25}
-      />
-    ),
-  });
-  static defaultProps = {
-    numColumns: 4,
+const Reset = (props) => (
+  <MaterialCommunityIcons
+    name="restart"
+    onPress={() =>
+      Alert.alert('Reset chart', 'Data for this chart will be cleared', [
+        { text: 'Cancel', style: 'cancel' },
+        {
+          text: 'Reset',
+          style: 'destructive',
+          onPress: () => props.resetChart(props.navPath),
+        },
+      ])
+    }
+    size={25}
+  />
+);
+
+const ConnectedReset = connect(null, { resetChart })(Reset);
+
+const Chart = (props) => {
+  const {
+    data, navigation, numColumns, defaultItemImage,
+  } = props;
+
+  const { images, mainImage, navPath } = props.screenProps;
+
+  const getItem = (item) => {
+    let Item;
+    if (
+      item.replace === undefined ||
+      (typeof item.replace === 'object' && item.replace.length === 0)
+    ) {
+      Item = <DefaultItem main={item.main} />;
+    } else if (typeof item.replace === 'object') {
+      Item = (
+        <ImageBackground source={defaultItemImage} style={styles.dataImage}>
+          {item.replace.map((index) => (
+            <Image key={index} style={styles.fittedImage} source={images[index]} />
+          ))}
+        </ImageBackground>
+      );
+    } else {
+      Item = <ImageBackground style={styles.dataImage} source={images[item.replace]} />;
+    }
+    return Item;
   };
 
-  componentDidMount() {
-    this.props.navigation.setParams({
-      reset: () =>
-        Alert.alert('Reset chart', 'Data for this chart will be cleared', [
-          { text: 'Cancel', style: 'cancel' },
-          {
-            text: 'Reset',
-            style: 'destructive',
-            onPress: () => this.props.resetChart(this.props.screenProps.navPath),
-          },
-        ]),
-    });
-  }
+  return (
+    <FlatList
+      ListHeaderComponent={<Image style={styles.image} source={mainImage} />}
+      data={Object.values(data || {})}
+      keyExtractor={(item, index) => index}
+      numColumns={numColumns}
+      ItemSeparatorComponent={() => (
+        <View
+          style={{
+            backgroundColor: 'white',
+            height: 0.5,
+          }}
+        />
+      )}
+      renderItem={({ item, index }) => (
+        <TouchableOpacity
+          style={[styles.view, (index + 1) % ITEMS_PER_ROW > 0 && styles.borderRight]}
+          onPress={() =>
+            navigation.navigate(navPath, {
+              day: item.main,
+              replace: item.replace,
+              onChange: (replaceIndex) => {
+                props.updateChart({ chart: navPath, index, replaceIndex });
+              },
+            })
+          }
+        >
+          {getItem(item)}
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
 
-  render() {
-    const { props } = this;
-    const {
-      data, navigation, numColumns, defaultItemImage,
-    } = props;
+Chart.navigationOptions = ({ screenProps: { navPath } }) => ({
+  headerRight: <ConnectedReset navPath={navPath} />,
+});
 
-    const { images, mainImage, navPath } = props.screenProps;
-
-    const getItem = (item) => {
-      let Item;
-      if (
-        item.replace === undefined ||
-        (typeof item.replace === 'object' && item.replace.length === 0)
-      ) {
-        Item = <DefaultItem main={item.main} />;
-      } else if (typeof item.replace === 'object') {
-        Item = (
-          <ImageBackground source={defaultItemImage} style={styles.dataImage}>
-            {item.replace.map((index) => (
-              <Image key={index} style={styles.fittedImage} source={images[index]} />
-            ))}
-          </ImageBackground>
-        );
-      } else {
-        Item = <ImageBackground style={styles.dataImage} source={images[item.replace]} />;
-      }
-      return Item;
-    };
-
-    return (
-      <FlatList
-        ListHeaderComponent={<Image style={styles.image} source={mainImage} />}
-        data={Object.values(data || {})}
-        keyExtractor={(item, index) => index}
-        numColumns={numColumns}
-        ItemSeparatorComponent={() => (
-          <View
-            style={{
-              backgroundColor: 'white',
-              height: 0.5,
-            }}
-          />
-        )}
-        renderItem={({ item, index }) => (
-          <TouchableOpacity
-            style={[styles.view, (index + 1) % ITEMS_PER_ROW > 0 && styles.borderRight]}
-            onPress={() =>
-              navigation.navigate(navPath, {
-                day: item.main,
-                replace: item.replace,
-                onChange: (replaceIndex) => {
-                  props.updateChart({ chart: navPath, index, replaceIndex });
-                },
-              })
-            }
-          >
-            {getItem(item)}
-          </TouchableOpacity>
-        )}
-      />
-    );
-  }
-}
+Chart.defaultProps = {
+  numColumns: 4,
+};
 
 export default connect(
   (state, ownProps) => ({
     data: state.profile.profiles[state.profile.activeProfile][ownProps.screenProps.navPath],
   }),
-  { resetChart, updateChart },
+  { updateChart },
 )(Chart);
