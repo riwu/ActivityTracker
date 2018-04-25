fe08e1f5259702f6b3fba4af63ca9a576eaacb41
riwu <riwu0730@gmail.com>
2018-04-25 11:31:22 +0800

    change status bar to dark theme

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { View, StatusBar, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { addNavigationHelpers } from 'react-navigation';
 import { addListener } from '../../App';
 import Drawer from './Drawer';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
 const AppWithNavigationState = ({ dispatch, navigation }) => (
-  <Drawer navigation={addNavigationHelpers({ dispatch, state: navigation, addListener })} />
+  <View style={styles.container}>
+    <StatusBar barStyle="dark-content" />
+    <Drawer navigation={addNavigationHelpers({ dispatch, state: navigation, addListener })} />
+  </View>
 );
 
 const mapStateToProps = (state) => ({
