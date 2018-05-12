83543a4a7e40b4494688754040a7ad8800ec8ea5
riwu <riwu0730@gmail.com>
2018-05-12 22:24:22 +0800

    update proverbs styling

--- src/components/DuaList.js
+++ src/components/DuaList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet, SectionList } from 'react-native';
+import { Text, StyleSheet, SectionList, View } from 'react-native';
 import proverbs from '../data/proverbs.json';
 
 const styles = StyleSheet.create({
@@ -8,23 +8,58 @@ const styles = StyleSheet.create({
   },
   header: {
     fontWeight: 'bold',
-    backgroundColor: 'green',
     fontSize: 20,
     padding: 10,
   },
-  item: {
+  itemContainer: {
     marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+  },
+  itemText: {
     fontSize: 15,
   },
+  content: {
+    flex: 1,
+  },
+  attribution: {
+    textAlign: 'right',
+    marginLeft: 10,
+  },
 });
+
+const COLORS = [
+  '#c3e17f',
+  '#f6fccc',
+  '#ffcc70',
+  '#f0776c',
+  '#da9dbe',
+  '#c49bdd',
+  '#6999e1',
+  '#61c2e3',
+];
 
 const DuaList = () => (
   <SectionList
     style={styles.container}
-    sections={proverbs}
-    renderItem={({ item, index }) => <Text style={styles.item}>{`${index + 1}. ${item}`}</Text>}
-    renderSectionHeader={({ section: { title } }) => <Text style={styles.header}>{title}</Text>}
-    keyExtractor={(item) => item}
+    sections={proverbs.map((proverb, index) => ({ ...proverb, index }))}
+    renderItem={({ item, index }) => (
+      <View style={styles.itemContainer}>
+        <Text style={[styles.itemText, styles.content]}>
+          {`${index + 1}. ${typeof item === 'string' ? item : item.content}`}
+        </Text>
+        {typeof item !== 'string' && (
+          <Text style={[styles.itemText, styles.attribution]}>{item.attribution}</Text>
+        )}
+      </View>
+    )}
+    renderSectionHeader={({ section }) => (
+      <Text style={[styles.header, { backgroundColor: COLORS[section.index % COLORS.length] }]}>
+        {section.title}
+      </Text>
+    )}
+    keyExtractor={(item) => (typeof item === 'string' ? item : item.content)}
   />
 );
 
